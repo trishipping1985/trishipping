8eb38d9f6ddc491f7c89f9e480e212c1dd5636e0
trishipping1985 <cath21marx@gmail.com>
2026-03-20 23:22:14 -0400

    Delete layout.tsx

--- app/dashboard/admin/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AdminGate from "@/components/AdminGate";
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <AdminGate redirectTo="/dashboard">{children}</AdminGate>;
-}
