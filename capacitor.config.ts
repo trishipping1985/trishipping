31b1e9ece9214fb64f48df1aca56fa1b7e45b759
trishipping1985 <cath21marx@gmail.com>
2026-03-28 14:56:37 -0400

    chore : initialize capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.trishipping.app',
+  appName: 'TRI Shipping',
+  webDir: 'public'
+};
+
+export default config;
