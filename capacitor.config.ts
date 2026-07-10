9530dd2472cd6ccf62cab9cb2920d3f608994e24
leen <leen@Tonys-MacBook-Air.local>
2026-07-10 05:23:36 +0300

    Fix iOS APNs registration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.trishipping.app",
+  appId: "info.trishipping.twa",
   appName: "TRI Shipping",
   webDir: "out",
   server: {
